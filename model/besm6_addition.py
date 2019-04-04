c6200437ca75e1330c25bfed4a33015cdac9ebc9
Serge Vakulenko <serge.vakulenko@gmail.com>
2019-04-03 20:51:26 -0700

    Implement AMX in besm6_addition.py script.

--- model/besm6_addition.py
+++ model/besm6_addition.py
@@ -1,11 +1,55 @@
 #!/usr/bin/python
 # -*- encoding: utf-8 -*-
 
+import sys
 from math import frexp, ldexp
 
-a = '4614000000000000' # 3072
-b = '4550000000000000' # 1024
-op = 'a-x' # 'a+x' 'a-x' 'x-a'
+if len(sys.argv) != 4:
+    print "Add two numbers in BESM-6 format"
+    print "Usage:"
+    print "        besm6_addition op num1 num2"
+    print "Parameters:"
+    print "        op           -- a+x, a-x, x-a or amx"
+    print "        num1, num2   -- 16-digit octal number or float number"
+    print "For example:"
+    print "        besm6_addition a-x 4614000000000000 4550000000000000"
+    print "or:"
+    print "        besmword x-a 3072.0 1024.0"
+    sys.exit(1)
+
+op = sys.argv[1]
+a = sys.argv[2]
+b = sys.argv[3]
+
+def besm_word(input):
+    if not ('.' in input) and not ('e' in input) and not ('E' in input):
+        return int(input, 8)
+    (fmantissa, exponent) = frexp(float(input))
+    sign = 0
+    if fmantissa > 0:
+        imantissa = int(fmantissa * (1<<40))
+        if imantissa < (1<<39):
+            raise Exception("Денормализованное число")
+        if exponent > 63:
+            raise Exception("Переполнение порядка")
+        if exponent < -64:
+            raise Exception("Опустошение порядка")
+        return imantissa | (exponent+64) << 41
+    elif fmantissa < 0:
+        imantissa = int((fmantissa + 1.0) * (1<<40))
+        if imantissa < 0:
+            raise Exception("Денормализованное отрицательное число")
+        if imantissa >= (1<<39):
+            # Normalize.
+            imantissa = int((fmantissa*2 + 1.0) * (1<<40))
+            exponent -= 1
+        if exponent > 63:
+            raise Exception("Переполнение порядка")
+        if exponent < -64:
+            raise Exception("Опустошение порядка")
+        return imantissa | (1<<40) | (exponent+64) << 41
+    else:
+        return 0
 
 def b6_to_str(ival):
     bits = "{:048b}".format(ival)
@@ -19,8 +63,10 @@ def b6_to_str(ival):
     return "{}".format("%.14g" % ldexp(fm, e - 64))
 
 print "Аргументы:"
-print "A = {} = {}".format(a, b6_to_str(int(a,8)))
-print "B = {} = {}".format(b, b6_to_str(int(b,8)))
+a = besm_word(a)
+b = besm_word(b)
+print "A = {:016o} = {}".format(a, b6_to_str(a))
+print "B = {:016o} = {}".format(b, b6_to_str(b))
 
 def summation(a1, a2, trace):
     sum_bits   = [c == '1' for c in a1][::-1]
@@ -41,7 +87,7 @@ def summation(a1, a2, trace):
             sum_bits[i]   = temp
             if carry_bits[i]:
                 carry_set = True
-        
+
         if trace: print "  Co = {}".format(''.join(['1' if c else '0' for c in carry_bits[::-1]]))
         if carry_set:
             if trace: print "  Был перенос, сдвигаем регистр переносов"
@@ -55,14 +101,14 @@ def summation(a1, a2, trace):
 
 print "\nРаспаковываем внутр. представление"
 print "Для числа A:"
-a = '{:048b}'.format(int(a, 8))
+a = '{:048b}'.format(a)
 ae = a[0:7]
 am = a[7:]
 print "\texponent = {} = {:d} = 2^{:d}".format(ae, int(ae,2), int(ae,2)-64)
 print "\tmantissa = {} {}".format(am[0], am[1:])
 
 print "Для числа B:"
-b = '{:048b}'.format(int(b, 8))
+b = '{:048b}'.format(b)
 be = b[0:7]
 bm = b[7:]
 print "\texponent = {} = {:d} = 2^{:d}".format(be, int(be,2), int(be,2)-64)
@@ -94,6 +140,16 @@ elif op == 'x-a':
     print "\nВыполняем обратное вычитание B-A"
     print "Для этого обращаем A"
     ae, am = negate(ae, am)
+elif op == 'amx':
+    print "\nВыполняем вычитание модулей |A|-|B|"
+    if am[0] == '1':
+        print "Обращаем A"
+        ae, am = negate(ae, am)
+    if bm[0] == '0':
+        print "Обращаем B"
+        be, bm = negate(be, bm)
+else:
+    raise Exception("Неправильная операция")
 
 print "\nВыравниваем порядки"
 rmr = '0'
@@ -129,9 +185,9 @@ bm = bm[0] + bm
 print "\tA m = {}".format(am)
 print "\tB m = {}".format(bm)
 
-
 print "\nВыполняем сложение мантисс"
 sum_ab = summation(am ,bm, True)
+
 
 print "\nВыполняем анализ 42,41-го бита: {}".format(sum_ab[:2])
 if sum_ab[0] != sum_ab[1]:
@@ -170,7 +226,7 @@ def normalize(e,m,rmr):
         count += 1
     e = '{:07b}'.format(e)
     return (e,m,rmr)
-        
+
 print "\nВыполняем нормализацию результата"
 e,m,rmr = normalize(ae, sum_ab, rmr)
 print "\texponent = {} = {:d} = 2^{:d}".format(e, int(e,2), int(e,2)-64)
@@ -187,5 +243,6 @@ else:
 print "\texponent = {} = {:d} = 2^{:d}".format(e, int(e,2), int(e,2)-64)
 print "\tmantissa = {} {}".format(m[0], m[1:])
 
+r = int(e+m,2)
 print "\nРезультат:"
-print "  {}".format(b6_to_str(int(e+m,2)))
+print "  {:016o} = {}".format(r, b6_to_str(r))
